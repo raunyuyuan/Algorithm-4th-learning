5175bb9106532bae9774e6400f05d5c4d1bddd0d
ruanyuyuan <765426599@qq.com>
2020-03-30 12:45:57 +0800

    update

---
+++ src/chapter1/二分查找/BinarySearch.java
@@ -0,0 +1,26 @@
+package chapter1.二分查找;
+import edu.princeton.cs.algs4.*;
+import java.util.Arrays;
+public class BinarySearch {
+    public static int indexOf(int[] a, int key) {
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo <= hi) {
+            // Key is in a[lo..hi] or not present.
+            int mid = lo + (hi - lo) / 2;
+            if      (key < a[mid]) hi = mid - 1;
+            else if (key > a[mid]) lo = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        int[] whitelist = in.readAllInts();
+        while (!StdIn.isEmpty()) {
+            int key = StdIn.readInt();
+            if (BinarySearch.indexOf(whitelist, key) == -1)
+                StdOut.println(key);
+        }
+    }
+}
